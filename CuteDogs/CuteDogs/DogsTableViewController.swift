a9ebc34f8ea1273ba58735b77106c2740cf05322
adam smith <adaminhonolulu@gmail.com>
2017-05-15 18:12:12 -1000

    adds table view for dogs.

--- CuteDogs/CuteDogs/DogsTableViewController.swift
+++ CuteDogs/CuteDogs/DogsTableViewController.swift
@@ -10,7 +10,25 @@ import UIKit
 import CoreData
 
 class DogsTableViewController: UITableViewController {
-
+    
+    var dogs = [Dog]()
     var managedObjectContext: NSManagedObjectContext!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let fetchRequest = NSFetchRequest<Dog>(entityName: "Dog")
+        dogs = try! managedObjectContext.fetch(fetchRequest)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dogs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cuteDogCell", for: indexPath)
+        let dog = dogs[indexPath.row]
+        cell.textLabel?.text = dog.name
+        return cell
+    }
 }
