02e5d6c0c26af8f6ecd155da4e9479d5de531bb3
adam smith <adaminhonolulu@gmail.com>
2017-05-15 18:05:14 -1000

    adds ability to insert a Dog.

--- CuteDogs/CuteDogs/AddDogViewController.swift
+++ CuteDogs/CuteDogs/AddDogViewController.swift
@@ -13,6 +13,23 @@ class AddDogViewController: UIViewController {
 
     var managedObjectContext: NSManagedObjectContext!
     
+    @IBOutlet weak var dogNameTextField: UITextField!
+    
+    @IBAction func handleCreateDogButtonTap(_ sender: Any) {
+        guard let name = dogNameTextField.text else { return }
+        createAndSaveDog(with: name)
+    }
+    
+    private func createAndSaveDog(with name: String) {
+        
+        guard let dog = NSEntityDescription.insertNewObject(forEntityName: "Dog", into: managedObjectContext) as? Dog else {
+            fatalError()
+        }
+        dog.name = name
+        try! managedObjectContext.save()
+        dogNameTextField.text = nil
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "segueFromAddDogsToViewDogs" {
             guard let dogsTableViewController = segue.destination as? DogsTableViewController else { fatalError() }
